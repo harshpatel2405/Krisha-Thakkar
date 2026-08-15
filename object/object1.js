ab87e440b17cda7c0327702c985bb1272c0c6730
harshpatel2405 <harshpatel4504@gmail.com>
2026-08-15 12:43:31 +0530

    dom2

---
+++ object/object1.js
@@ -0,0 +1,18 @@
+const data = {
+  name: "harsh",
+  age: 22,
+  marks: 90,
+  address: {
+    pincode: 344566,
+    city: "AHM",
+  },
+};
+const arr =[]
+console.log(data)
+console.log(data.name)
+console.log(data.address)
+console.log(data.address.pincode)
+
+arr.push(data)
+console.log(arr)
+
